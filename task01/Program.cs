42ef555f22c637aa155293294abe4fd1fdc74fc1
Rayenire <aeristarg@gmail.com>
2022-06-24 15:31:01 +0300

    Add data

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA * numberA);
