65fe0d7c5543091be70acbf235975db2757b8d76
Rayenire <aeristarg@gmail.com>
2022-06-25 19:44:56 +0300

    Add 1 hometask

---
+++ homework01/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA > numberB){
+    Console.Write("max = ");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write("max = ");
+    Console.Write(numberB);
+}
