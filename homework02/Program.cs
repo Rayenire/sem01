db1b912d66257a17057f05e207403b6f02e5f086
Rayenire <aeristarg@gmail.com>
2022-06-25 21:05:53 +0300

    Add 2 hometask but not ended

---
+++ homework02/Program.cs
@@ -0,0 +1,17 @@
+﻿int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+int C = Convert.ToInt32(Console.ReadLine());
+
+if(B > A){
+    Console.Write("max = ");
+    Console.WriteLine(B);
+}
+if(C > A){
+    Console.Write("max = ");
+    Console.WriteLine(C);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(A);
+}
